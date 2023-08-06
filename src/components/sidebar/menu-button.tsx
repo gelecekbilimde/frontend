99f1eecb2a7ef9fdc6f5487104fc22a73f644033
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-08-06 21:44:01 +0300

    default icon added to category menu buttons

--- src/components/sidebar/menu-button.tsx
+++ src/components/sidebar/menu-button.tsx
@@ -1,18 +1,20 @@
+import { CircleDashedIcon } from 'lucide-react';
+
 interface Properties extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: JSX.Element;
-  secondIcon?: JSX.Element;
   isChild?: boolean;
 }
 
-export default function MenuButton({ children, icon, secondIcon, isChild, onClick }: Properties): JSX.Element {
+export default function MenuButton({ children, isChild, icon, onClick }: Properties): JSX.Element {
   return (
     <button
       onClick={onClick}
-      className="flex min-w-[160px] gap-2 rounded-lg bg-cream px-2 py-1 transition-colors duration-200 hover:bg-neutral-300"
+      className={`flex max-h-7 min-w-[160px] items-center gap-2 rounded-lg bg-cream px-2 py-1 text-sm transition-colors duration-200 hover:bg-neutral-300 ${
+        isChild ? 'ps-4' : ''
+      }`}
     >
-      <div className="h-4 w-4">{icon}</div>
-      <div className={`text-sm ${isChild ? 'ps-2' : ''}`}>{children}</div>
-      <div className="h-4 w-4">{secondIcon}</div>
+      {icon ?? <CircleDashedIcon height={16} width={16} />}
+      {children}
     </button>
   );
 }

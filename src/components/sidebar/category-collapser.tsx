35d891c8847f9d30e35e12cab9c8c995cb825daf
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-08-06 21:45:31 +0300

    category menu expand icon added, type separated

--- src/components/sidebar/category-collapser.tsx
+++ src/components/sidebar/category-collapser.tsx
@@ -1,20 +1,31 @@
+'use client';
+
 import { useState } from 'react';
+import { ChevronRightIcon } from 'lucide-react';
+
 import MenuButton from './menu-button';
 
-export default function CategoryCollapser({
-  title,
-  size = 2,
-  children,
-}: {
+interface Properties {
   title: string;
   size?: number;
-  children: React.ReactNode;
-}) {
+  children: JSX.Element | JSX.Element[];
+}
+
+export default function CategoryCollapser({ title, size = 2, children }: Properties) {
   const [state, setState] = useState<boolean>(false);
+  const height = 32 * size;
   return (
     <>
-      <MenuButton onClick={() => setState((state: boolean) => !state)}>{title}</MenuButton>
-      <div className="max-h-0 overflow-hidden transition-all" style={{ maxHeight: state ? `${size * 28}px` : '0px' }}>
+      <MenuButton
+        onClick={() => setState((state: boolean) => !state)}
+        icon={<ChevronRightIcon className={`transition-all ${state ? 'rotate-90' : ''}`} height={16} width={16} />}
+      >
+        {title}
+      </MenuButton>
+      <div
+        className="max-h-0 gap-2 overflow-hidden transition-all"
+        style={{ maxHeight: state ? `${height}px` : '0px' }}
+      >
         {children}
       </div>
     </>

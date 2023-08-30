4ae8dda2aaf8be26d343ec988d72442ec19b31fa
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-08-30 13:36:28 +0300

    feat: implement hydrating, create basic post page

---
+++ src/app/post/[id]/_components/single-post.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { getPosts } from '../_services/get-post';
+
+export default function SinglePost({ id }: { id: number }): JSX.Element {
+  const key = ['posts', id];
+
+  const { data: post } = useQuery({ queryKey: key, queryFn: async () => await getPosts(id) });
+
+  return (
+    <div>
+      <h1>{post?.title}</h1>
+      <p>{post?.body}</p>
+    </div>
+  );
+}
---
+++ src/app/post/[id]/page.tsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/named
+import { dehydrate, Hydrate } from '@tanstack/react-query';
+import getQueryClient from '@/lib/getQueryClient';
+
+import { getPosts } from './_services/get-post';
+import SinglePost from './_components/single-post';
+
+interface Properties {
+  params: {
+    id: number;
+  };
+}
+
+export default async function Post({ params }: Properties): Promise<JSX.Element> {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(['posts', params.id], async () => await getPosts(params.id));
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <SinglePost id={params.id} />
+    </Hydrate>
+  );
+}

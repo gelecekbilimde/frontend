1a2e7ce9a2a97afb89373b65fffaa18e838eb246
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-04 20:42:27 +0300

    fix: change main layout

--- src/app/(layout)/_components/MainLayout/index.tsx
+++ src/app/(layout)/_components/MainLayout/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Header from '@/app/(layout)/header';
-import SideBar from '@/app/(layout)/sidebar';
+import LeftSideBar from '@/app/(layout)/left-sidebar';
 
-export default function Index({ children }: { children: React.ReactNode }) {
+export default function Index({ children }: { children: React.ReactNode }): JSX.ELement {
   return (
-    <div className="max-w-screen-2xl w-full flex items-center flex-col justify-between">
+    <div className="flex w-full max-w-screen-2xl flex-col items-center justify-between">
       <Header />
-      <div className="flex justify-between w-full my-10">
-        <SideBar />
-        <main>{children}</main>
-        <div>
-            asdasdadsasdasd
+      <div className="my-10 flex w-full flex-row justify-between">
+        <div className="w-2/12">
+          <LeftSideBar />
         </div>
+        <main className="w-7/12">{children}</main>
+        <div className="w-2/12">Right Sidebar</div>
       </div>
     </div>
   );
 }
-

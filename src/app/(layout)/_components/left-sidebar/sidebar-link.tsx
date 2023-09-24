d19dde8b53d1d0db7daf2633400cc1d6cffba55a
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-24 10:16:18 +0300

    feat: add sidebar button and link

---
+++ src/app/(layout)/_components/left-sidebar/sidebar-link.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from 'react';
+import Icon from '@/lib/get-luicide-icon';
+import type dynamicIconImports from 'lucide-react/dynamicIconImports';
+import Link from 'next/link';
+import SidebarButton from './sidebar-button';
+
+interface Properties {
+  slug: string;
+  icon: keyof typeof dynamicIconImports;
+  children: ReactNode;
+}
+
+export default function SidebarLink({ slug, icon, children }: Properties): JSX.Element {
+  return (
+    <Link href={`category/${slug}`}>
+      <SidebarButton leftIcon={<Icon name={icon} color="black" size={16} />}>{children}</SidebarButton>
+    </Link>
+  );
+}

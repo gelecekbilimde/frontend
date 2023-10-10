6832fdb2f43ac548d604be996df9483f6b15064c
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-10 15:31:04 +0300

    refactor: relocate files

--- src/app/[user]/(profile)/followers/[page]/page.tsx
+++ src/app/[user]/(profile)/followers/[page]/page.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 
-import mock_data from './_assets/MOCK_DATA.json';
-import FollowerCard from './_component/follower-card';
+import mock_data from '../../_assets/MOCK_DATA.json';
+import FollowerCard from './follower-card';
 
 interface Properties {
   params: {
@@ -23,7 +23,9 @@ export default function Followers({ params }: Properties): JSX.Element {
       <h1 className="py-2 ps-4 text-2xl font-bold">Followers</h1>
       <div className="flex flex-row flex-wrap">
         {mock_data.slice(perPage * (currentPage - 1), perPage * (currentPage - 1) + perPage).map((item) => (
-          <FollowerCard key={item.id} follower={item} />
+          <div key={item.id} className="w-6/12 p-2">
+            <FollowerCard follower={item} />
+          </div>
         ))}
       </div>
       <div className="flex w-full justify-center gap-4 py-2">

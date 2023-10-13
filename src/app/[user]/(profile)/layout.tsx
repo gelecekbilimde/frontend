e2ecacac0532e130d30071998c91ac68327b9ba5
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-13 18:08:55 +0300

    feat: add layout to profile page

--- src/app/[user]/(profile)/layout.tsx
+++ src/app/[user]/(profile)/layout.tsx
@@ -1,3 +1,5 @@
+import Header from '@/app/(layout)/header';
+
 import ProfileHeader from './_components/profile-header';
 import Sidebar from './_components/sidebar';
 
@@ -17,13 +19,21 @@ const user = {
 export default function Layout({ params, children }: Properties) {
   // TODO: Fetch user data from API
   return (
-    <div className="flex w-full max-w-screen-lg flex-col gap-4 py-8">
-      <ProfileHeader user={user} />
-      <div className="flex w-full flex-col gap-4 md:flex-row">
-        <div className="flex w-full flex-col gap-4 md:w-1/3">
-          <Sidebar />
-        </div>
-        <div className="flex w-full flex-col gap-4 md:w-2/3">{children}</div>
+    <div className="flex w-full max-w-screen-2xl flex-col items-center justify-between">
+      <Header />
+      <div className="my-10 flex w-full flex-row justify-between">
+        <main className="w-10/12">
+          <div className="flex w-full flex-col gap-4 py-8">
+            <ProfileHeader user={user} />
+            <div className="flex w-full flex-col gap-4 md:flex-row">
+              <div className="flex w-full flex-col gap-4 md:w-1/3">
+                <Sidebar />
+              </div>
+              <div className="flex w-full flex-col gap-4 md:w-2/3">{children}</div>
+            </div>
+          </div>
+        </main>
+        <div className="w-2/12">Right Sidebar</div>
       </div>
     </div>
   );

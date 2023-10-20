f6807c1dd572adfd65ed784596b5348f32df3af6
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-20 17:40:29 +0300

    refactor: change profile layout from flex to grid

--- src/app/[user]/(profile)/layout.tsx
+++ src/app/[user]/(profile)/layout.tsx
@@ -19,26 +19,26 @@ const user = {
   image: 'https://picsum.photos/128',
 };
 
-export default function Layout({ params, children }: Properties) {
+export default function Layout({ children }: Properties) {
   // TODO: Fetch user data from API
   return (
     <div className="flex w-full max-w-screen-2xl flex-col items-center justify-between">
       <Header />
-      <div className="my-10 flex w-full flex-row justify-between">
-        <main className="w-10/12">
-          <div className="flex w-full flex-col gap-4 py-8">
+      <div className="my-10 grid w-full grid-cols-6 flex-row justify-between gap-2">
+        <main className="col-span-6 md:col-span-5">
+          <div className="flex w-full flex-col gap-4">
             <ProfileHeader user={user} />
-            <div className="flex w-full flex-col gap-4 md:flex-row">
-              <div className="flex w-full flex-col gap-4 md:w-1/3">
+            <div className="grid w-full grid-cols-3 gap-4">
+              <div className="col-span-3 flex flex-col gap-4 md:col-span-1">
                 <Sidebar />
               </div>
-              <div className="flex w-full flex-col gap-4 md:w-2/3">
+              <div className="col-span-3 flex flex-col gap-4 md:col-span-2">
                 <Suspense fallback={<Loading />}>{children}</Suspense>
               </div>
             </div>
           </div>
         </main>
-        <div className="w-2/12">Right Sidebar</div>
+        <div className="col-span-1 hidden md:flex md:flex-col">Right Sidebar</div>
       </div>
     </div>
   );

4a6667a7f2b473a869bed2ded2ca397c0bfccb46
HkaanUsta <egemen.kaan@hotmail.com>
2024-08-23 16:54:48 +0300

    feat: rightsidebar responsive

--- src/components/layout/main-layout/index.tsx
+++ src/components/layout/main-layout/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import RightSidebar from "@/components/layout/right-sidebar";
 
 import Header from "../header";
+import LeftSideBar from "../left-sidebar";
 
 export default function Index({
   children,
@@ -13,15 +14,15 @@ export default function Index({
   className?: string;
 }): JSX.Element {
   return (
-    <div className="flex w-full max-w-screen-2xl flex-col items-center justify-between px-14">
+    <div className="flex w-full max-w-screen-2xl flex-col items-center justify-between px-5 lg:px-14">
       <Header />
       <div className="my-10 grid grid-cols-12 gap-x-5">
-        <div className="col-span-2">
-          Sidebar
-          {/* <LeftSideBar /> */}
+        <div className="col-span-2 hidden lg:block">
+          
+          <LeftSideBar />
         </div>
-        <main className={cx("col-span-8 mx-10", className)}>{children}</main>
-        <div className="col-span-2">
+        <main className={cx("sm:col-span-8 md:col-span-9 lg:col-span-7 col-span-12 mr-2 lg:mx-10", className)}>{children}</main>
+        <div className="col-span-4 hidden lg:col-span-3 md:col-span-3 sm:block">
           <RightSidebar />
         </div>
       </div>

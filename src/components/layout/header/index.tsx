6d63af5572539d52ca263c00cb390b01edb53a34
Ali Osman Delismen <dlsmnosman@gmail.com>
2024-08-31 21:31:33 +0300

    fix: build errors

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,39 +1,16 @@
 "use client";
 
+import "react-modern-drawer/dist/index.css";
+
 import { MenuIcon, SearchIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import Drawer from "react-modern-drawer";
-import "react-modern-drawer/dist/index.css";
-
-import ProfileDropdown from "./profile-dropdown";
-import CategorySelectDrawer from "../drawers/category-select-drawer";
-
-const links = [
-  {
-    id: 1,
-    name: "Ana Sayfa",
-    path: "/",
-  },
-  {
-    id: 3,
-    name: "Video İçerikler",
-    path: "/videos",
-  },
-  {
-    id: 4,
-    name: "Podcastler",
-    path: "/podcasts",
-  },
-  {
-    id: 5,
-    name: "İletişim",
-    path: "/contact",
-  },
-];
-
-import { Navigations, NavigationsVariant } from "./navigations";
 import { useState } from "react";
+import Drawer from "react-modern-drawer";
+
+import CategorySelectDrawer from "../drawers/category-select-drawer";
+import { Navigations, NavigationsVariant } from "./navigations";
+import ProfileDropdown from "./profile-dropdown";
 
 export default function Header(): JSX.Element {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -43,28 +20,31 @@ export default function Header(): JSX.Element {
   };
 
   return (
-    <header className="flex h-20 w-full items-center justify-between">
-      <CategorySelectDrawer />
-      <Link href="/">
-        <Image src="/images/logo.png" alt="logo" width={200} height={42} />
-      </Link>
-        
+    <>
+      <header className="flex h-20 w-full items-center justify-between">
+        <CategorySelectDrawer />
+        <Link href="/">
+          <Image src="/images/logo.png" alt="logo" width={200} height={42} />
+        </Link>
+
         <nav className="hidden md:block">
           <Navigations variant={NavigationsVariant.Desktop} />
         </nav>
 
-        <div className="flex-row-reverse hidden md:flex md:flex-row gap-x-5">
-          <button className="flex h-10 w-10 items-center justify-center rounded-lg border border-slate-200 bg-slate-100">
-            <SearchIcon className="h-4 w-4 text-slate-600" />
+        <div className="hidden flex-row-reverse gap-x-5 md:flex md:flex-row">
+          <button className="size-10 flex items-center justify-center rounded-lg border border-slate-200 bg-slate-100">
+            <SearchIcon className="size-4 text-slate-600" />
           </button>
           <ProfileDropdown />
         </div>
-        <div className="md:hidden flex">
-          <button className="flex h-10 w-10 items-center justify-center rounded-lg border mr-2 border-slate-200 bg-slate-100">
-            <SearchIcon className="h-4 w-4 text-slate-600" />
+        <div className="flex md:hidden">
+          <button className="size-10 mr-2 flex items-center justify-center rounded-lg border border-slate-200 bg-slate-100">
+            <SearchIcon className="size-4 text-slate-600" />
           </button>
-          <button onClick={toggleDrawer} className="flex h-10 w-10 items-center justify-center rounded-lg border border-slate-200 bg-slate-100">
-            <MenuIcon className="h-4 w-4 text-slate-600" />
+          <button
+            onClick={toggleDrawer}
+            className="size-10 flex items-center justify-center rounded-lg border border-slate-200 bg-slate-100">
+            <MenuIcon className="size-4 text-slate-600" />
           </button>
         </div>
       </header>

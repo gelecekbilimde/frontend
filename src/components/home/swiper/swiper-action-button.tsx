da5792e311dcc3ea786d1b2b286bcc962f2df768
Hasret Sultan Ünal <s.hasretunal@gmail.com>
2024-09-06 15:54:57 +0300

    feat: make swiper responsive

--- src/components/home/swiper/swiper-action-button.tsx
+++ src/components/home/swiper/swiper-action-button.tsx
@@ -9,11 +9,12 @@ const SwiperActionButton = (properties: {
 }) => {
   return (
     <div
-      className={`grid h-11 w-40 cursor-pointer grid-cols-8 items-center justify-center gap-x-2 rounded-md border border-white/10 bg-[#42494D]/40 px-3 text-[8px] text-white backdrop-blur-sm ${properties.class}`}
+      className={`flex flex-wrap h-11 w-40 cursor-pointer items-center justify-center gap-x-2 rounded-md border border-white/10 bg-[#42494D]/40 px-3 text-[8px] text-white backdrop-blur-sm ${properties.class}`}
     >
-      <span className="col-span-8 line-clamp-2 text-white">
+      <span className="line-clamp-2 text-white max-md:hidden">
         {swiperData[properties.index].title}
       </span>
+      <span className="md:hidden">{properties.index+1}</span>
     </div>
   );
 };

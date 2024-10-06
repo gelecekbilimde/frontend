a9d90cfd4857d9d28d4c92cfd60d2efde69e6c6a
Hasret Sultan Ünal <s.hasretunal@gmail.com>
2024-10-06 15:35:29 +0300

    fix: change icons

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import { Eye, Heart, Share2 } from "lucide-react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useState } from "react";
@@ -27,6 +26,7 @@ import {
 } from "@/components/ui/popover";
 import { defaultPostsConstants } from "@/constants/post.constants";
 import { MemoizedMarkdown } from "@/components/article/markdown";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const PostDetailPage = (): JSX.Element => {
   const { data, isLoading } = useQuery({
@@ -94,12 +94,12 @@ const PostDetailPage = (): JSX.Element => {
           <button
             className="flex size-8 items-center justify-center rounded-md bg-red-500"
             onClick={handleLike}>
-            <Heart color="white" fill={like ? "#EF4444" : "white"} size={19} />
+              <Icon icon="ri:heart-fill" color={like ? "#EF4444" : "white"} stroke={like ? "white" : "#EF4444"} strokeWidth={2} height={19} />
           </button>
           <Popover>
             <PopoverTrigger asChild>
               <button className="flex size-8 items-center justify-center rounded-md bg-green-500">
-                <Share2 color="white" size={16} />
+                <Icon icon="lucide:share-2" color="white" height={16} />
               </button>
             </PopoverTrigger>
             <PopoverContent side="top" className="flex w-52 gap-1">
@@ -121,7 +121,7 @@ const PostDetailPage = (): JSX.Element => {
             </PopoverContent>
           </Popover>
           <div className="flex items-center gap-2 text-slate-500">
-            <Eye size={16} />
+            <Icon icon="lucide:eye" height={16} />
             <span className="text-xs">
               <span className="font-bold">{data?.view}</span> görüntüleme
             </span>
